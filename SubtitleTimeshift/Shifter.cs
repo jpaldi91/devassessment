93a01e0f05569d34c278f4b41cd79901918d9a3f
JP Diniz <jpaldi91@gmail.com>
2023-09-11 16:53:47 -0300

    WIP: stops before the end of original file

--- SubtitleTimeshift/Shifter.cs
+++ SubtitleTimeshift/Shifter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,27 @@ namespace SubtitleTimeshift
     {
         async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
         {
-            throw new NotImplementedException();
+            StreamReader reader = new StreamReader(input, encoding, true, bufferSize);
+            StreamWriter writer = new StreamWriter(output, encoding, bufferSize, leaveOpen);
+            int lineNumber;
+
+            while (int.TryParse(reader.ReadLine(), out lineNumber))
+            {
+                string[] interval = reader.ReadLine().Split(" --> ", 2);
+                List<string> textLines = new List<string>();
+                string text = reader.ReadLine();
+                while (!string.IsNullOrWhiteSpace(text))
+                {
+                    textLines.Add(text);
+                    text = reader.ReadLine();
+                }
+                string startString = interval[0].Replace(',', '.');
+                string endString = interval[1].Replace(',', '.');
+                TimeSpan start = TimeSpan.Parse(startString) + timeSpan;
+                TimeSpan end = TimeSpan.Parse(endString) + timeSpan;
+                Subtitle subtitle = new Subtitle(lineNumber, start, end, textLines);
+                writer.Write(subtitle.ToString());
+            }
         }
     }
 }
---
+++ SubtitleTimeshift/Subtitle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SubtitleTimeshift;
+
+public class Subtitle
+{
+  public Subtitle(int lineNumber, TimeSpan start, TimeSpan end, List<string> text)
+  {
+    LineNumber = lineNumber;
+    Start = start;
+    End = end;
+    Text = text;
+  }
+  public int LineNumber { get; set; }
+  public TimeSpan Start { get; set; }
+  public TimeSpan End { get; set; }
+  public List<string> Text { get; set; }
+
+  public override string ToString()
+  {
+    List<string> lines = new List<string>();
+    string timeSpanFormat = @"hh\:mm\:ss\.fff";
+    string startString = Start.ToString(timeSpanFormat);
+    string endString = End.ToString(timeSpanFormat);
+
+    lines.Add(LineNumber.ToString());
+    lines.Add($"{startString} --> {endString}");
+    lines.AddRange(Text);
+    lines.Add(System.Environment.NewLine);
+
+    return string.Join(System.Environment.NewLine, lines);
+  }
+}
